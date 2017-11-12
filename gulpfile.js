bb55b6344fad8052cdce9c0a150a534c9a613492
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-11-12 01:10:01 -0600

    Converts styles.scss to CSS with gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,9 @@ var sass = require('gulp-sass');
 gulp.task('hello', function() {
   console.log('Hello Zell');
 });
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+});
