d8bf3c090a3053533f5a206d0841e8ff58673fa2
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-11-12 02:43:44 -0600

    updates images task to cache images already optimized

--- gulpfile.js
+++ gulpfile.js
@@ -49,9 +49,9 @@ gulp.task('useref', function(){
 
 gulp.task('images', function(){
   return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-  .pipe(imagemin({
-      // Setting interlaced to true
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
       interlaced: true
-    }))
+    })))
   .pipe(gulp.dest('dist/images'))
 });
