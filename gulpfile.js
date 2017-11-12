8b3f2a60e1e9eb52ccd205c4cfbedff6124bee47
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-11-12 03:11:11 -0600

    adds build task

--- gulpfile.js
+++ gulpfile.js
@@ -70,3 +70,10 @@ gulp.task('clean:dist', function() {
 gulp.task('cache:clear', function (callback) {
 return cache.clearAll(callback)
 });
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+});
