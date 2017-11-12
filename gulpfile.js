eefe0ee70df1b528f08cf0027a45589f41d6cd2a
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-11-12 01:22:29 -0600

    missing semicolon

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
-})
+});
 
 gulp.task('watch', function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
