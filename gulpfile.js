52f0920358e7af73072e87b5826da8bec5760869
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-11-12 02:16:07 -0600

    defines useref task

--- gulpfile.js
+++ gulpfile.js
@@ -31,3 +31,9 @@ gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
 });
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
+});
