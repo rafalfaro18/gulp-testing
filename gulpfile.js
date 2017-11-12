f242e8d6c8e9282f5c276a97b3911515b1648ea1
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-11-12 01:15:17 -0600

    Gets files ending with scss in app/scss and compiles them with gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ gulp.task('hello', function() {
   console.log('Hello Zell');
 });
 
-gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
     .pipe(gulp.dest('app/css'))
-});
+})
